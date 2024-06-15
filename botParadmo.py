a096e628c64df4e7cf5f335b291ce754e6d97507
Adriano Serafim <adriano_serafim_1@hotmail.com>
2024-06-15 08:44:43 -0300

    Foi adicionado uma nova função ao bot, a função de ataque

--- botParadmo.py
+++ botParadmo.py
@@ -1,11 +1,27 @@
-import pyautogui
-import time
+import pyautogui # Serve para presonar alguma tecla do seu teclado para você
+import time # Macador de tempo
+import threading # Faz com que as funções funcionem ao mesmo tempo
 
-# Função para pressionar a tecla 'v' a cada 10 segundos
-def press_v_every_10_seconds():
+# Função para pressionar a tecla 'A' a cada 1.9 segundos
+def press_a_every_1_9_seconds():
+    while True:
+        pyautogui.press('a')
+        time.sleep(1.34)
+
+# Função para pressionar a tecla 'V' a cada 10 segundos
+def press_v_every_1_second():
     while True:
         pyautogui.press('v')
         time.sleep(1)
 
-# Chama a função
-press_v_every_10_seconds()
+# Criação das threads para executar as funções simultaneamente
+thread_a = threading.Thread(target=press_a_every_1_9_seconds)
+thread_v = threading.Thread(target=press_v_every_1_second)
+
+# Início das threads
+thread_a.start()
+thread_v.start()
+
+# Espera que as threads terminem (na verdade, isso não acontecerá porque os loops são infinitos)
+thread_a.join()
+thread_v.join()
