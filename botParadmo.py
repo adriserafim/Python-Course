7fc9a3b6d488cdd31261b85360ae430d7aab5c69
Adriano Serafim <adriano_serafim_1@hotmail.com>
2024-06-16 12:58:44 -0300

    Adição de teclas para acionamento como f1,f2,f3 e f4

--- botParadmo.py
+++ botParadmo.py
@@ -3,20 +3,44 @@ import time # Macador de tempo
 import threading # Faz com que as funções funcionem ao mesmo tempo
 
 # Função para pressionar a tecla 'A' a cada 1.9 segundos
-def press_a_every_1_9_seconds():
+def press_a_every():
     while True:
         pyautogui.press('a')
         time.sleep(1.34)
 
 # Função para pressionar a tecla 'V' a cada 10 segundos
-def press_v_every_1_second():
+def press_v_every():
     while True:
         pyautogui.press('v')
         time.sleep(1)
 
+def press_f1_every():
+    while True:
+        pyautogui.press('q')
+        time.sleep(2.5)
+
+def press_f2_every():
+    while True:
+        pyautogui.press('w')
+        time.sleep(2.5)
+
+def press_f3_every():
+    while True:
+        pyautogui.press('e')
+        time.sleep(2.5)
+
+def press_f4_every():
+    while True:
+        pyautogui.press('r')
+        time.sleep(2.5)
+
 # Criação das threads para executar as funções simultaneamente
-thread_a = threading.Thread(target=press_a_every_1_9_seconds)
-thread_v = threading.Thread(target=press_v_every_1_second)
+thread_a = threading.Thread(target=press_a_every)
+thread_v = threading.Thread(target=press_v_every)
+thread_f1 = threading.Thread(target=press_f1_every)
+thread_f2 = threading.Thread(target=press_f2_every)
+thread_f3 = threading.Thread(target=press_f3_every)
+thread_f4 = threading.Thread(target=press_f4_every)
 
 # Início das threads
 thread_a.start()
