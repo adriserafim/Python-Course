0936f9da27d272bc74fb249d315f7f254102dd92
Adriano Serafim <adriano_serafim_1@hotmail.com>
2024-07-02 16:44:11 -0300

    Foi auterado as funções de clique para que possa ter um inicializador e um finalizador, foi colocado um metodo main, mas não foi adapidato para todas as funções ainda e tambem não foi feito uma função muito complexa

--- botParadmo.py
+++ botParadmo.py
@@ -1,15 +1,20 @@
 import pyautogui # Serve para presonar alguma tecla do seu teclado para você
 import time # Macador de tempo
 import threading # Faz com que as funções funcionem ao mesmo tempo
+import keyboard
 
-control = 2
-time_attack = 2
+control = 1
+time_attack = 5
 time_skill_1 = 4
-time_skill_2 = 2.25
-time_skill_3 = 2.25
+time_skill_2 = 13
+time_skill_3 = 40
 time_skill_4 = 2.25
 time_drop = 1
 time_wait = 1
+
+# Evento para controlar a execução da thread
+stop_event = threading.Event()
+thread_a = None  # Variável global para a thread
 
 # Função para pressionar a tecla 'A' a cada 1.9 segundos
 def press_a_every():
@@ -49,48 +54,132 @@ def press_f4_every():
         time.sleep(time_skill_4)
 
 # Criação das threads para executar as funções simultaneamente
-thread_a = threading.Thread(target=press_a_every)
-thread_v = threading.Thread(target=press_v_every)
-thread_f1 = threading.Thread(target=press_f1_every)
-thread_f2 = threading.Thread(target=press_f2_every)
-thread_f3 = threading.Thread(target=press_f3_every)
-thread_f4 = threading.Thread(target=press_f4_every)
+# Função para iniciar a thread
+def start_thread_a():
+    global thread_a
+    stop_event.clear()  # Reseta o evento de parada
+    thread_a = threading.Thread(target=press_a_every)
+    thread_a.start()
 
-while control <=10:
-    if control == 1:
+def start_thread_v():
+    global thread_v
+    stop_event.clear()  
+    thread_v = threading.Thread(target=press_v_every)
+    thread_v.start()
+
+def start_thread_f1():
+    global thread_f1
+    stop_event.clear()  
+    thread_f1 = threading.Thread(target=press_f1_every)
+    thread_f1.start()
+
+def start_thread_f2():
+    global thread_f2
+    stop_event.clear()  
+    thread_f2 = threading.Thread(target=press_f2_every)
+    thread_f2.start()
+
+def start_thread_f3():
+    global thread_f3
+    stop_event.clear()  
+    thread_f3 = threading.Thread(target=press_f3_every)
+    thread_f3.start()
+
+def start_thread_f4():
+    global thread_f4
+    stop_event.clear()  
+    thread_f4 = threading.Thread(target=press_f4_every)
+    thread_f4.start()
+
+# Função para parar a thread
+def stop_thread_a():
+    stop_event.set()  # Sinaliza a thread para parar
+    if thread_a is not None:
+        thread_a.join()  # Espera que a thread termine
+
+def stop_thread_v():
+    stop_event.set()  
+    if thread_v is not thread_v:
+        thread_v.join()  
+
+def stop_thread_f1():
+    stop_event.set()  
+    if thread_f1 is not thread_f1:
+        thread_f1.join()  
+
+def stop_thread_f2():
+    stop_event.set()  
+    if thread_f2 is not thread_f2:
+        thread_f2.join()  
+
+def stop_thread_f3():
+    stop_event.set()
+    if thread_f3 is not thread_f3:
+        thread_f3.join()
+
+def stop_thread_f4():
+    stop_event.set()
+    if thread_f4 is not thread_f4:
+        thread_f4.join()
+
+# Função principal
+def main():
+    print("Pressione '-' para iniciar e ';' para parar.")
+
+    while control <=10:
+        #if control == 1:
         # Início das threads
-        thread_a.start()
-        thread_v.start()
+            #thread_a.start()
+            #thread_v.start()
 
         # Espera que as threads terminem (na verdade, isso não acontecerá porque os loops são infinitos)
-        thread_a.join()
-        thread_v.join()
+            #thread_a.join()
+            #thread_v.join()
 
-    elif control == 2:
-        thread_f1.start()
-        thread_a.start()
-        thread_v.start()
+        #elif control == 2:
+            #thread_f1.start()
+            #thread_a.start()
+            #thread_v.start()
 
-        thread_f1.join()
-        thread_a.join()
-        thread_v.join()
+            #thread_f1.join()
+            #thread_a.join()
+            #thread_v.join()
     
-    elif control == 3:
-        thread_a.start()
-        thread_v.start()
-        thread_f1.start()
-        time.sleep(time_wait)
-        thread_f2.start()
+        #elif control == 3:
+            #thread_a.start()
+            #thread_v.start()
+            #thread_f1.start()
+            #time.sleep(time_wait)
+            #thread_f2.start()
 
-        thread_a.join()
-        thread_v.join()
-        thread_f1.join()
-        thread_f2.join()
+            #thread_a.join()
+            #thread_v.join()
+            #thread_f1.join()
+            #thread_f2.join()
     
-    else:
-        print(f"O control está igua a {control}. O valor dessa variaável sera mandado para 11") # O f serve para colocar o valor de uma variável no texto. Para fazer isso a variável tem que estar entre {} 
-        control == 11
+        #else:
+            #print(f"O control está igua a {control}. O valor dessa variaável sera mandado para 11") # O f serve para colocar o valor de uma variável no texto. Para fazer isso a variável tem que estar entre {} 
+            #control == 11
+        
+        if control == 1:
+            if keyboard.is_pressed('-'):
+                if thread_a is None or not thread_a.is_alive():
+                    print("Iniciando...")
+                    start_thread_a()
+                    print(f"Pressionando a tecla 'a' a cada {time_attack} minuto.")
+                    while keyboard.is_pressed('-'):  # Espera a tecla ser liberada
+                        pass
+            if keyboard.is_pressed(';'):
+                if thread_a is not None and thread_a.is_alive():
+                    print("Parando...")
+                    start_thread_a()
+                    print("Parado.")
+                    while keyboard.is_pressed(';'):  # Espera a tecla ser liberada
+                        pass
+            time.sleep(0.1)  # Pequena pausa para evitar uso excessivo da CPU
 
+if __name__ == "__main__":
+    main()
 
 # Início das threads
 #thread_a.start()
