7c769bc3fad3ecb8f3d31fc5c5b6cdc3562418d6
Adriano Serafim <adriano_serafim_1@hotmail.com>
2024-08-25 20:26:37 -0300

    Foi adicinado um codificação diretente para o clique pois não estava funcionando o clique simples, programação bem sucedida! Foi colocado mas detalhes no código para deixar o seu uso mais confortavel e dinamico

--- clickforme.py
+++ clickforme.py
@@ -2,12 +2,29 @@ import pyautogui
 import time
 import openpyxl
 
-while True:
-    # Clique com o botão direito do mouse na posição atual do cursor
-    pyautogui.rightClick()
+looping = 0
+MAXlooping= 0
+
+# Um tempo antes de começar
+time.sleep(5)
+while looping < MAXlooping:
+    # Clique simples com o botão direito do mouse na posição atual do cursor
+    #pyautogui.rightClick() # Não funcionou então não mantivemos o projeto
+
+    # Pressiona o botão direito do mouse
+    pyautogui.mouseDown(button='right')
+
+    # Aguarda um pouco para simular o tempo que o botão direito está pressionado (pode ser 0 se não quiser um delay aqui)
+    time.sleep(0.5)
+
+    # Solta o botão direito do mouse
+    pyautogui.mouseUp(button='right')
 
     # Aguarda 3,5 segundos
-    time.sleep(3.5)
+    time.sleep(0.5)
 
     # Pressiona a tecla Enter
     pyautogui.press('enter')
+
+    # Aguarda alguns segundos
+    time.sleep(1.5)
