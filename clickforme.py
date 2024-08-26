f6445e2353c323d7b794a6c38245644a3d4f2763
Adriano Serafim <adriano_serafim_1@hotmail.com>
2024-08-25 21:35:36 -0300

    Foi adicionad mais dinamismo ao código e foi acertado o tempo

--- clickforme.py
+++ clickforme.py
@@ -3,30 +3,34 @@ import time
 import openpyxl
 
 looping = 0
-MAXlooping= 150
+MAXlooping= 145
+tipo = 1
 
-# Um tempo antes de começar
-time.sleep(5)
-while looping < MAXlooping:
-    looping += 1 # Isso é para adicionar 1 ao numero para uma hora acabar com o looping
+# Essa parte do código é para abrir loots na bolsa ou banco
+if tipo == 1:
+    # Um tempo antes de começar
+    time.sleep(5)
+    
+    while looping < MAXlooping:
+        looping += 1 # Isso é para adicionar 1 ao numero para uma hora acabar com o looping
 
-    # Clique simples com o botão direito do mouse na posição atual do cursor
-    #pyautogui.rightClick() # Não funcionou então não mantivemos o projeto
+        # Clique simples com o botão direito do mouse na posição atual do cursor
+        #pyautogui.rightClick() # Não funcionou então não mantivemos o projeto
 
-    # Pressiona o botão direito do mouse
-    pyautogui.mouseDown(button='right')
+        # Pressiona o botão direito do mouse
+        pyautogui.mouseDown(button='right')
 
-    # Aguarda um pouco para simular o tempo que o botão direito está pressionado (pode ser 0 se não quiser um delay aqui)
-    time.sleep(0.5)
+        # Aguarda um pouco para simular o tempo que o botão direito está pressionado (pode ser 0 se não quiser um delay aqui)
+        time.sleep(0.1)
 
-    # Solta o botão direito do mouse
-    pyautogui.mouseUp(button='right')
+        # Solta o botão direito do mouse
+        pyautogui.mouseUp(button='right')
 
-    # Aguarda 3,5 segundos
-    time.sleep(0.5)
+        # Aguarda 3,5 segundos
+        time.sleep(0.2)
 
-    # Pressiona a tecla Enter
-    pyautogui.press('enter')
+        # Pressiona a tecla Enter
+        pyautogui.press('enter')
 
-    # Aguarda alguns segundos
-    time.sleep(1.5)
+        # Aguarda alguns segundos
+        time.sleep(1.2)
